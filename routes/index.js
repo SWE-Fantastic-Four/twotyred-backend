4ef78534b139a13f118c5a1c447aeda81e62a18b
kaijun123 <kaijun123@gmail.com>
2022-09-26 14:30:46 +0800

    Added createUser api

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import test from "./test.js";
 import saveRoute from "./saveRoute.js";
+import createUser from "./createUser.js"
+import updateUsername from "./updateUsername.js"
 
 
 const router = Router();
 router.use("/test", test);
 router.use("/routes/save", saveRoute);
+router.use("/user/create", createUser); // TODO: change to /user/:username/create
+router.use("/user/username", updateUsername);
 
 
 
--- routes/saveRoute.js
+++ routes/saveRoute.js
@@ -3,12 +3,11 @@ import admin from "firebase-admin";
 import { Routes } from "../firebase.js";
 
 
-
 const router = Router();
 
-const save = async (userName, timestamp, routesGeometry, duration, time, likes) => {
+const save = async (username, timestamp, routesGeometry, duration, time, likes) => {
   await Routes.add({
-    Username: userName,
+    Username: username,
     Timestamp: timestamp,
     Geometry: routesGeometry,
     Distance: duration,
@@ -19,15 +18,15 @@ const save = async (userName, timestamp, routesGeometry, duration, time, likes) 
 
 
 router.post("/", async (req, res) => {
-  const { userName, routesGeometry, distance, duration, likes } = req.body;
+  const { username, routesGeometry, distance, duration, likes } = req.body;
   const timestamp = admin.firestore.FieldValue.serverTimestamp();
   console.log(timestamp);
   // const durationNum = parseFloat(duration);
   // const timeNum = parseInt(time);
   // const likesNum = parseInt(likes);
   try {
-    await save(userName, timestamp, routesGeometry, distance, duration, likes);
-    res.send("Route Successfully saved");
+    await save(username, timestamp, routesGeometry, distance, duration, likes);
+    res.status(200).send("Route Successfully saved");
   } catch (error) {
     console.log(error.message);
     res.status(400).send("Unable to save route")
