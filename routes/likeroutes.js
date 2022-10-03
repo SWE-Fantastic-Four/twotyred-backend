a15635887983533845c5d06bcea570d8391a5710
evonneng05 <evonnenglilin@gmail.com>
2022-10-03 14:14:53 +0800

    "Edited like routes function"

--- routes/likeroutes.js
+++ routes/likeroutes.js
@@ -1,32 +1,28 @@
 import admin from "firebase-admin";
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-exports.likeRoute = functions.https.onCall((data, context) => {
-    if (!context) {
-        throw new functions.https.HttpsError(
-            'you must be a registered user to like route'
-        );
-    }
-    // get refs for user doc and route doc
-    const user = admin.firestore().collection('Users').doc(context.username);
-    const route = admin.firestore().collection('Routes').doc(data.id); // how to get id of a route?
-    // how to tell likes to be an array?
-    return user.get().then(doc => {
-        // check that the user hasn't already liked the post
-        if (doc.data().Likes.inclues(data.id)) {
-            throw new functions.https.HttpsError(
-                'You can ony like a post once',
-            );
-        }
-        return user.update({
-            Likes: [...doc.data().Likes, data.id]
-        })
-            .then(() => {
-                return route.update({
-                    Likes: admin.firestore.FieldValue.increment(1)
-                });
-        })
-    })
-})
+exports.likeRoute = functions.https.onCall(async (data, context) => {
+  if (!context) {
+    throw new functions.https.HttpsError(
+      "you must be a registered user to like route"
+    );
+  }
+  // get refs for user doc and route doc
+  const user = admin.firestore().collection("Users").doc(context.username);
+  const route = admin.firestore().collection("Routes").doc(data.id); // how to get id of a route?
+  // how to tell likes to be an array?
+  const doc = await user.get();
+  // check that the user hasn't already liked the post
+  if (doc.data().Likes.inclues(data.id)) {
+    throw new functions.https.HttpsError("You can only like a post once");
+  }
+  await user.update({
+    Likes: [...doc.data().Likes, data.id],
+  });
+
+  return route.update({
+    Likes: admin.firestore.FieldValue.increment(1),
+  });
+});
