37a904242b3a544522287c64f1214c4f59cbb775
changdaozheng <50071666+changdaozheng@users.noreply.github.com>
2022-10-05 19:49:06 +0800

    Standardised scripts with feat/likeroutes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ import test from "./test.js";
 import saveRoute from "./saveRoute.js";
 import createUser from "./createUser.js"
 import updateUsername from "./updateUsername.js"
-import handleFavourite from "./handleFavourite.js"
+import favouriteRoute from "./favouriteRoute.js"
+import unfavouriteRoute from "./unfavouriteRoute.js"
 
 
 const router = Router();
@@ -11,9 +12,7 @@ router.use("/test", test);
 router.use("/routes/save", saveRoute);
 router.use("/user/create", createUser); // TODO: change to /user/:username/create
 router.use("/user/update/username", updateUsername);
-router.use("/handlefavourite", handleFavourite);
-
-
-
+router.use("/routes/:routeId/favourite", favouriteRoute);
+router.use("/routes/:routeId/unfavourite", unfavouriteRoute)
 
 export default router;
---
+++ routes/unfavouriteRoute.js
@@ -0,0 +1,47 @@
+import { Router } from "express";
+import { Routes, User } from "../firebase.js";
+import { FieldValue } from "@google-cloud/firestore";
+
+const router = Router();
+
+function fsArrayInclude(fsArray, searchItem){
+  
+    for (let i = 0; i < fsArray.length; i++) {
+        if( fsArray[i] === searchItem){
+            return true;
+        }
+      }
+    return false;
+}
+
+router.post("/", async (req, res)=>{
+
+    const userRef = User.doc(req.body.user);
+    const routeRef = Routes.doc(req.body.route);
+
+    try{
+        const userSnapshot = await userRef.get();
+        const routeSnapshot = await routeRef.get();
+        
+        if(!userSnapshot.exists || !routeSnapshot.exists){
+            throw new Error("unsuccessful: invalid user or route");
+        }
+
+        const userData = userSnapshot.data();
+        const userFav = userData.Favourites;
+
+        if(!fsArrayInclude(userFav, req.body.route)) {
+            throw new Error("unsuccessful: route not saved as favourite")
+        }
+
+        const updateUserRes = await userRef.update({Favourites: FieldValue.arrayRemove(req.body.route)})
+        console.log(updateUserRes);
+        res.status(200).send("successful: route removed from favourite");
+        
+    } catch(err){
+        console.log(err);
+        res.status(400).send(err.message);
+    }
+})
+
+export default router;
