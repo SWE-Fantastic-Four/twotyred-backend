615f834fbbaa5b396f7ed4974769fafc57e93e30
changdaozheng <50071666+changdaozheng@users.noreply.github.com>
2022-10-05 19:03:23 +0800

    Removed duplicated router

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ import test from "./test.js";
 import saveRoute from "./saveRoute.js";
 import createUser from "./createUser.js"
 import updateUsername from "./updateUsername.js"
-import handlelikes from "./handleLike.js"
 import likeRoutes from "./likeRoutes.js"
 import unlikeRoutes from "./unlikeRoutes.js"
 
@@ -13,7 +12,6 @@ router.use("/test", test);
 router.use("/routes/save", saveRoute);
 router.use("/user/create", createUser); // TODO: change to /user/:username/create
 router.use("/user/update/username", updateUsername);
-router.use("/handlelikes", handlelikes);
 router.use("/routes/like", likeRoutes);
 router.use("/routes/unlike", unlikeRoutes);
 
