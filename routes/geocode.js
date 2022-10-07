d169cd353eaa15351c8b69aaf444ae352c2095ad
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-10-07 12:28:58 +0800

    Added Geocoding API

---
+++ routes/geocode.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import getOneMapAuth from "../scripts/getOneMapAuth.js";
+import fetch from "node-fetch";
+
+const router = Router();
+
+router.post("/", async(req, res) => {
+  const { lat, lng } = req.body;
+  try {
+    const token = await getOneMapAuth();
+    const fetchResponse = await fetch(`https://developers.onemap.sg/privateapi/commonsvc/revgeocode?location=${lat},${lng}&token=${token}`);
+    let resJSON = await fetchResponse.json();
+    resJSON = resJSON["GeocodeInfo"][0];
+    const resObj = {}
+    if ("BUILDINGNAME" in resJSON) {
+      resObj["address"] = resJSON["BUILDINGNAME"]
+    } else if ("ROAD" in resJSON) {
+      resObj["address"] = resJSON["ROAD"]
+    } else if ("FEATURE_NAME" in resJSON) {
+      resObj["address"] = resJSON["FEATURE_NAME"]
+    } else {
+      throw new Error("Invalid set of coordinates"); 
+    }
+    res.status(200).json(resObj);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+export default router;
--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,19 @@ import favouriteRoute from "./favouriteRoute.js"
 import unfavouriteRoute from "./unfavouriteRoute.js"
 import likeRoutes from "./likeRoutes.js"
 import unlikeRoutes from "./unlikeRoutes.js"
-
+import geocode from "./geocode.js";
 
 const router = Router();
 router.use("/test", test);
 router.use("/routes/save", saveRoute);
 router.use("/user/create", createUser); // TODO: change to /user/:username/create
 router.use("/user/update/username", updateUsername);
-router.use("/planroute", planRoute);
 router.use("/routes/:routeId/favourite", favouriteRoute);
 router.use("/routes/:routeId/unfavourite", unfavouriteRoute)
 router.use("/routes/like", likeRoutes);
 router.use("/routes/unlike", unlikeRoutes);
+router.use("/planroute", planRoute);
+router.use("/geocode", geocode);
 
 
 export default router;
