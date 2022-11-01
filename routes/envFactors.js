9162955476423bff9f668f4fea63209f2f18415b
changdaozheng <50071666+changdaozheng@users.noreply.github.com>
2022-11-01 18:10:31 +0800

    Fixed day formatting

--- routes/envFactors.js
+++ routes/envFactors.js
@@ -14,7 +14,9 @@ router.post("/", async (req, res)=>{
     const currDate = date.toLocaleDateString('en-GB', { dateStyle: "medium", timeZone: "Asia/Singapore"});
     let currTime = date.toLocaleTimeString('en-GB', { timeStyle: "medium", timeZone: "Asia/Singapore"});
     const month = new Date().toLocaleString('en-US', { month: '2-digit' });
-    const [day,, year] = currDate.split(" ");
+    var [day,, year] = currDate.split(" ");
+    day = day < 10 ? '0' + day : day;
+ 
     const [hours, mins] = currTime.split(":");
     currTime = date.toLocaleTimeString('en-US', {timeZone: 'Asia/Singapore', hour12: true, hour: 'numeric', minute: 'numeric'});
 
@@ -24,6 +26,8 @@ router.post("/", async (req, res)=>{
     const weatherURL = `https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=${year}-${month}-${day}T${hours}%3A${mins}%3A00`;
     const weatherRes = await fetch(weatherURL);
     const weatherJSON = await weatherRes.json();
+    console.log(weatherURL);
+    console.log(weatherJSON);
     const regions = weatherJSON['area_metadata'];
     const forecasts = weatherJSON['items'][0]["forecasts"];
     
@@ -48,6 +52,7 @@ router.post("/", async (req, res)=>{
     const temperatureURL = `https://api.data.gov.sg/v1/environment/air-temperature?date_time=${year}-${month}-${day}T${hours}%3A${mins}%3A00`;
     const temperatureRes = await fetch(temperatureURL);
     const temperatureJSON = await temperatureRes.json();
+    
     const stations = temperatureJSON['metadata']['stations'];
     const readings = temperatureJSON['items'][0]["readings"];
     
